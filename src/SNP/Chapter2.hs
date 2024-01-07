9e7aad76ccb2e1cbb8d87ce55785977743e920dd
Deniz Alp Durmaz <denalp@proton.me>
2024-01-07 17:46:13 +0200

    Add repeatUntil

--- src/SNP/Chapter2.hs
+++ src/SNP/Chapter2.hs
@@ -3,11 +3,13 @@
 module SNP.Chapter2 where
 
 ------------------------------------------------------------------------------
+-- lsp-execute-code-action
 
 import Control.Monad (unless)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Resource (runResourceT)
 import Data.Char (isDigit, toUpper)
+import Data.List (nub)
 import Data.Text (Text)
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
@@ -31,9 +33,51 @@ capitalizeLast = go
           Text.snoc front $ toUpper back
 
 ------------------------------------------------------------------------------
+unParen :: Text -> Maybe Text
+unParen t = case Text.stripPrefix (Text.pack "(") t of
+  Just t' -> Text.stripSuffix (Text.pack ")") t'
+  Nothing -> Nothing
+
+------------------------------------------------------------------------------
+characterCount :: FilePath -> IO Int
+characterCount fp = runResourceT $ do
+  dir <- liftIO getCurrentDirectory
+  (_, h) <- fileResource (dir </> fp) ReadMode
+  liftIO (hCharacterCount h)
+
+------------------------------------------------------------------------------
+hCharacterCount :: Handle -> IO Int
+hCharacterCount h = proceed 0
+  where
+    proceed n = do
+      x <- Text.hGetChunk h
+      case Text.null x of
+        True -> return n
+        False -> proceed (n + Text.length x)
+
+------------------------------------------------------------------------------
+repeatUntil ::
+  (Monad m) =>
+  m chunk ->
+  (chunk -> Bool) ->
+  (chunk -> m ()) ->
+  m ()
+repeatUntil getChunk isEnd f = proceed
+  where
+    proceed = do
+      chunk <- getChunk
+      unless (isEnd chunk) $ f chunk
+      proceed
+
+------------------------------------------------------------------------------
 checks :: IO ()
 checks = do
   putStrLn "Exercise 4:"
   print $ digitsOnly $ Text.pack "ab c123 def4"
   putStrLn "Exercise 5:"
   print $ capitalizeLast $ Text.pack "dog"
+  putStrLn "Exercise 6:"
+  print $ unParen $ Text.pack "(cat)"
+  print $ unParen $ Text.pack "cat"
+  putStrLn "Exercise 7:"
+  print =<< characterCount "greeting.txt"
